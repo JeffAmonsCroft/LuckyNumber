0054f7f9b8dcfc84650f15ee4e97c7cf1c2b6c38
SEKATTE AMON <amonsekate@gmail.com>
2026-05-12 21:50:58 +0300

    Update urls.py

--- luckynumber/urls.py
+++ luckynumber/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.signup_view, name='signup'),
     path('luckynumber-signup', views.signup_view, name='signup'),
-    path('luckynumber-login', views.login_view, name='login'),
+    path('useraccounts/login/', views.login_view, name='login'),
     path('useraccounts/signup/', views.signup_view, name='signup'),
     path('pages/lose/', views.lose_view, name='lose'),
     path('win/', views.win_view, name='win'),
