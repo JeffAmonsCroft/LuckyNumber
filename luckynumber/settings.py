f6ced26af5bc3278bae13e3acc00c2a86e5dcecb
SEKATTE AMON <amonsekate@gmail.com>
2026-05-12 21:12:51 +0300

    Update settings.py

--- luckynumber/settings.py
+++ luckynumber/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n_5^udlq6!_)m@%ck8smu#g6!ip1%db)9k&!knxrqxhvnn(vq%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['luckynumber-bxc3.onrender.com']
 
 
 # Application definition
